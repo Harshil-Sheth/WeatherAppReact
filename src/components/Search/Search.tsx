69d94bb49760ca3b28ec003cf2ffd9f33361f438
Harshil Sheth <harshil.sheth@gatewaygroup.com>
2021-07-22 17:40:13 +0530

    unique search items in updated

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import React, { Dispatch, FC, SetStateAction, useState } from "react";
 import './Search.css'
 import cities from '../../cities.json'
 
@@ -11,7 +11,7 @@ const Search:FC<Props> = (props) => {
   
   const [input, setInput] = useState("");
   
-  const [searchedCities, setSearchedCities] = useState([]);
+  const [searchedCities, setSearchedCities] = useState<Array<String>>([]);
   const searchedCity = cities.filter(data=>data.name.toLowerCase().includes(input.toLowerCase()))
   
     function handleChange (e:React.ChangeEvent<HTMLInputElement>):void {
@@ -23,8 +23,12 @@ const Search:FC<Props> = (props) => {
         props.setcity(input.innerText)
         let existingCities:Array<String> = []
         localStorage.getItem("cities")!==null&&(JSON.parse(localStorage.getItem("cities")||"")).map((data:any)=>existingCities.push(data))
-        existingCities.push(input.innerText);
-        localStorage.setItem("cities", JSON.stringify(existingCities));
+        
+        existingCities.length<6?existingCities.push(input.innerText):existingCities.shift(); existingCities.push(input.innerText);
+        let newcities =existingCities&& new Set(existingCities)
+        let uniqueCities = Array.from(newcities);
+          localStorage.setItem("cities", JSON.stringify(uniqueCities));
+          uniqueCities&&setSearchedCities(uniqueCities);
         setInput('')
       }
       function clickEvent2(e: React.MouseEvent<HTMLDivElement>){
@@ -33,8 +37,11 @@ const Search:FC<Props> = (props) => {
         console.log(input.innerText)
         let existingCities:Array<String> = []
         localStorage.getItem("cities")!==null&&(JSON.parse(localStorage.getItem("cities")||"")).map((data:any)=>existingCities.push(data))
-        existingCities.push(input.innerText);
-        localStorage.setItem("cities", JSON.stringify(existingCities));
+        existingCities.length<6?existingCities.push(input.innerText):existingCities.shift(); existingCities.push(input.innerText);
+        let newcities =existingCities&& new Set(existingCities)
+      let uniqueCities = Array.from(newcities);
+        localStorage.setItem("cities", JSON.stringify(uniqueCities));
+        setSearchedCities(uniqueCities);
         setInput('')
       }
     let clear = input.length>1? 'fa fa-close icon2': 'none'
@@ -42,10 +49,13 @@ const Search:FC<Props> = (props) => {
     let searchList = input.length>1? 'suggestions': 'no-suggestions'
 
 
-    useEffect(() => {
-      let searchedCities = JSON.parse(localStorage.getItem("cities")||"")
-      setSearchedCities(searchedCities);
-    }, [])
+    // useEffect(() => {
+    //   let searchedCities = JSON.parse(localStorage.getItem("cities")||"")
+    //   let newcities =searchedCities&& new Set(searchedCities)
+    //   newcities = Array.from(newcities);
+    //   setSearchedCities(newcities);
+    //   console.log('fired')
+    // },[])
 
 
     return (
@@ -93,12 +103,17 @@ const Search:FC<Props> = (props) => {
                 className={clear}
               ></i>
             </div>
+            {searchedCities.length>0?
             <div style={{display:'flex',justifyContent:'center',margin:'5px',alignItems:'center'}}>
               Recent: 
             {searchedCities.map((data:any)=>
             
             <div className='recent' onClick={(e:React.MouseEvent<HTMLDivElement>)=>clickEvent2(e)} style={{display:'flex',justifyContent:'center',background: "rgba(255, 255, 255, 0.28)",height:'35px',margin:'5px',padding:'5px',borderRadius:'10px'}}>{data}</div>
             )}</div>
+            :
+           <></>
+            
+            }
           </div>
     )
 }
